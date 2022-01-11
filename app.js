44c1beb656d2dcb88640ee199d118d57735c4757
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-01-11 23:10:24 +0530

    updated app.js using ES6 arrow functions ➡️

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
-var textInput = document.querySelector("textarea")
+var textInput = document.querySelector("textarea");
 
-var buttonTranslate = document.querySelector("button")
+var buttonTranslate = document.querySelector("button");
 
-var textOutputDiv = document.querySelector(".output")
+var textOutputDiv = document.querySelector(".output");
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"; // to do
 
-var serverUrl = "https://api.funtranslations.com/translate/minion.json" // to do 
+//we have to make url construction function
 
-//we have to make url construction function 
+const getTranslationUrl = (input) => {
+  return serverUrl + "?" + "text=" + input;
+};
 
-function getTranslationUrl(input){
-    return serverUrl + "?" + "text=" + input
-}
+const errorHandler = (error) => {
+  console.log("Error Occured", error.message);
+  alert("Something went wrong please try again after sometime ");
+};
+//make a fetch function
 
-function errorHandler(error){
-    console.log("Error Occured", error.message )
-    alert("Something went wrong please try again after sometime ")
-}
-//make a fetch function 
+const clickHandler = () => {
+  var result = textInput.value; // taking input from user
 
-function clickHandler(){
-    var result = textInput.value // taking input from user
-
-    //calling server for processing 
-    fetch(getTranslationUrl(result))
-    .then(response => response.json()) // converting the response to json format using .json()
-    .then(json => {
-        // we get the value of the translated text and store it in a var
-        var translatedText = json.contents.translated
-        textOutputDiv.innerHTML = "<h1>" + translatedText +  "</h1>"
+  //calling server for processing
+  fetch(getTranslationUrl(result))
+    .then((response) => response.json()) // converting the response to json format using .json()
+    .then((json) => {
+      // we get the value of the translated text and store it in a var
+      var translatedText = json.contents.translated;
+      textOutputDiv.innerHTML = "<h1>" + translatedText + "</h1>";
     })
-    .catch(errorHandler)
-}
-
+    .catch(errorHandler);
+};
 
 // function output(){
 //     var result = textInput.value
 //     textOutputDiv.innerHTML = "<h1>" + result +  "</h1>"
 // }
 
-
-buttonTranslate.addEventListener("click", clickHandler)
+buttonTranslate.addEventListener("click", clickHandler);
